c05f89127aa41465cd7a9003bb2fbcb3089c65a1
caldeirav <vincent.caldeira@gmail.com>
2025-04-13 15:51:51 +0800

    Simple agent leveraging web search and model endpoint with RAG

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from smolagents import (
     LiteLLMModel,
 )
 
-# Declare the model for the web search agent
+# Declare the model for the web search agent and the orchestration agent
 model = LiteLLMModel(
-    model_id="text-completion-openai/granite-3.1-8b-instruct",
-    api_base="http://127.0.0.1:1234/v1",
+    model_id="text-completion-openai/llama-3.2-3b-instruct",
+    api_base="http://127.0.0.1:1234/v1/",
     api_key="YOUR_API_KEY", # replace with API key if necessary
     num_ctx=8096, # ollama default is 2048 which will fail horribly. 8096 works for easy tasks, more is better. Check https://huggingface.co/spaces/NyxKrage/LLM-Model-VRAM-Calculator to calculate how much VRAM this will need for the selected model.
 )
@@ -20,11 +20,17 @@ search_agent = ToolCallingAgent(
     description="This is an agent that can perform web search.",
 )
 
+#Create a CodeAgent that manages the Search Agent 
 manager_agent = CodeAgent(
     tools=[],
     model=model,
-    max_steps=5,
+    max_steps=3,
     verbosity_level=2,
     managed_agents=[search_agent],
 )
-manager_agent.run("If the US keeps it 2024 growth rate, how many years would it take for the GDP to double?")
+
+agent_output = manager_agent.run("What was NVIDIA total revenue in 2024?")
+
+print("Final output:")
+print(agent_output)
+
